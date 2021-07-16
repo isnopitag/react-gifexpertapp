90d76374baf160472a73530ba90836d438270a7f
Carmona <13030617@itcelaya.edu.mx>
2021-07-16 17:55:28 -0500

    pequeno cambio

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -2,31 +2,34 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const AddCategory = ({ setCategories }) => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const handeInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleSubmit = (e) => {
-      e.preventDefault();
-      if( inputValue.trim().length > 2 ){
-          setCategories( c => [inputValue,...c])
-          setInputValue('');
+    e.preventDefault();
+    if (inputValue.trim() === "conejinga") {
+      setCategories((c) => ["bunny", ...c]);
+      alert("Te quiero mucho mi conejita 🐇");
+      setInputValue("");
+    } else {
+      if (inputValue.trim().length > 2) {
+        setCategories((c) => [inputValue, ...c]);
+        setInputValue("");
       }
+    }
   };
   return (
-    <form onSubmit={ handleSubmit}>
-      <input 
-        type="text"
-        value={inputValue} 
-        onChange={handeInputChange} />
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handeInputChange} />
     </form>
   );
 };
 
 AddCategory.propTypes = {
-    setCategories: PropTypes.func,
-}
+  setCategories: PropTypes.func,
+};
 
 export default AddCategory;
